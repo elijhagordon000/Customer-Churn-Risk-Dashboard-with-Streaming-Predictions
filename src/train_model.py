8f23732fd208706f7d0caa3d7a2c8c8fda648a21
Elijha Gordon <elijha2001@gmail.com>
2026-06-15 17:40:15 -0500

    Defined the model structure

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import torch
+import torch.nn as nn
 
 # Project rooot = one level above src/
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
@@ -16,3 +17,12 @@ X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32) # torch.Size(
 X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32) # torch.Size([1409, 38])
 Y_train_tensor = torch.tensor(Y_train.values, dtype=torch.float32) # torch.Size([5634, 1])
 Y_test_tensor = torch.tensor(Y_test.values, dtype=torch.float32) # torch.Size([1409, 1])
+
+# Building a neural net
+
+model = nn.Sequential(
+    nn.Linear(X_train_tensor.shape[1], 64), # Input layer 38 -> 64 (64 is a hyperparameter)
+    nn.ReLU(), # Hidden Layer
+    nn.Linear(64,1) # Output layer 64 -> 1
+)
+
